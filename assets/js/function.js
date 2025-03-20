c54795272d69226438541027040a8d5652da996a
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2025-03-20 17:39:21 +0600

    Final project upload

--- assets/js/function.js
+++ assets/js/function.js
@@ -33,6 +33,63 @@ $(".counter").counterUp({
   time: 5000,
 });
 
+/* Contact form validation */
+var $contactform = $("#contactForm");
+$contactform.validator({ focus: false }).on("submit", function (event) {
+  if (!event.isDefaultPrevented()) {
+    event.preventDefault();
+    submitForm();
+  }
+});
+
+function submitForm() {
+  /* Initiate Variables With Form Content*/
+  var fullname = $("#fullname").val();
+  var email = $("#email").val();
+  var phone = $("#phone").val();
+  var subject = $("#subject").val();
+  var message = $("#msg").val();
+
+  $.ajax({
+    type: "POST",
+    url: "form-process.php",
+    data:
+      "fullname=" +
+      fullname +
+      "&name=" +
+      "&email=" +
+      email +
+      "&phone=" +
+      phone +
+      "&subject=" +
+      subject +
+      "&message=" +
+      message,
+    success: function (text) {
+      if (text == "success") {
+        formSuccess();
+      } else {
+        submitMSG(false, text);
+      }
+    },
+  });
+}
+
+function formSuccess() {
+  $contactform[0].reset();
+  submitMSG(true, "Message Sent Successfully!");
+}
+
+function submitMSG(valid, msg) {
+  if (valid) {
+    var msgClasses = "h3 text-success";
+  } else {
+    var msgClasses = "h3 text-danger";
+  }
+  $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+/* Contact form validation end */
+
 /* Appointment form validation */
 var $appointmentForm = $("#appointmentForm");
 $appointmentForm.validator({ focus: false }).on("submit", function (event) {
@@ -88,3 +145,7 @@ function appointmentsubmitMSG(valid, msg) {
   $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
 /* Appointment form validation end */
+
+// back to top button start //
+
+// back to top button end //
